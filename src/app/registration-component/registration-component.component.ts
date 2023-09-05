4cd918bc206c6dcb6a1d270683a298cd9f1ef4e3
Hariharan Elangovan <hariharan15elangovan@gmail.com>
2023-09-06 00:30:18 +0530

    web

--- src/app/registration-component/registration-component.component.ts
+++ src/app/registration-component/registration-component.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { FormControl,FormGroup,Validators}from '@angular/forms'
 @Component({
   selector: 'app-registration-component',
   templateUrl: './registration-component.component.html',
   styleUrls: ['./registration-component.component.css']
 })
 export class RegistrationComponentComponent {
-
+    ValidationForm!:FormGroup;
+    constructor(){
+     this.ValidationForm=new FormGroup({
+      email:new FormControl('admin@gmail.com',[Validators.required,Validators.email]),
+      password:new FormControl('admin1234',[Validators.required, Validators.minLength(8)])
+     });
+    }
+  
+    submit(){
+      console.log(this.ValidationForm.get('email')?.value);
+    }
 }
